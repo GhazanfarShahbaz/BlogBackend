f933b47860a163c232d0ac914b65260f14ac0f29
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-05-29 23:37:58 -0400

    update user model file

--- database/models/User.model.js
+++ database/models/User.model.js
@@ -1,29 +1,31 @@
 const { DataTypes } = require('sequelize');
 
-const User = sequelize.define('user', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
+    });
+};
