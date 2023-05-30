e242eab116c4f720d90f8b1cc2153abb8ed654a7
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-05-29 23:20:37 -0400

    Update connection logic

--- database/Connection.js
+++ database/Connection.js
@@ -1,4 +1,22 @@
 require('dotenv').config();
 
 const { Sequelize } = require("sequuelize");
-const CONNECTION_URL = process.env.DATABSE_URL
+const CONNECTION_URL = process.env.DATABSE_URL;
+
+
+const sequelize = new Sequelize(CONNECTION_URL);
+
+// https://github.com/sequelize/express-example/blob/master/express-main-example/sequelize/index.js
+const modelDefiners = [
+	require('./models/User.model'),
+	require('./models/BlogPost.model'),
+    // we add more models here when we create them
+];
+
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+	modelDefiner(sequelize);
+}
+
+// We export the sequelize connection instance to be used around our app.
+module.exports = sequelize;
