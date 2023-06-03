532fe16bd1192b79cfe0462f258b7592ff43be51
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-06-03 10:32:52 +0000

    update blog model store class

--- database/repository/blog_post_repository.js
+++ database/repository/blog_post_repository.js
@@ -3,11 +3,11 @@ class BlogPostStore {
         this.BlogPost = sequelize.models.blog_post;
     }
 
-    async insert(title, content, publishedAt = new Date(), editedAt = new Date()) {
-        return await this.BlogPost.create({ title, content, publishedAt, editedAt });
+    async insert(title, content, tags = [], publishedAt = new Date(), editedAt = new Date()) {
+        return await this.BlogPost.create({ title, content, publishedAt, editedAt, tags });
     }
 
-    async update(id, title, content, editedAt = new Date()) {
+    async update(id, title, content, tags = [], editedAt = new Date()) {
         const blogPost = await this.BlogPost.findByPk(id);
         if (!blogPost){
             throw new Error(`Cannot find blog post with id ${id}`);
@@ -16,6 +16,7 @@ class BlogPostStore {
         blogPost.title = title;
         blogPost.content = content;
         blogPost.editedAt = editedAt;
+        blogPost.tags = tags;
 
         return await blogPost.save();
     }
