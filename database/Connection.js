353a4141db5bb683e806debcc89914e0f97b8cbd
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-06-03 05:46:18 +0000

    fix typos and add function for creating tables on first go

--- database/Connection.js
+++ database/Connection.js
@@ -1,22 +1,29 @@
 require('dotenv').config();
 
-const { Sequelize } = require("sequuelize");
+const { Sequelize } = require("sequelize");
 const CONNECTION_URL = process.env.DATABSE_URL;
-
 
 const sequelize = new Sequelize(CONNECTION_URL);
 
 // https://github.com/sequelize/express-example/blob/master/express-main-example/sequelize/index.js
 const modelDefiners = [
-	require('./models/User.model'),
-	require('./models/BlogPost.model'),
+    require('./models/User.model'),
+    require('./models/BlogPost.model'),
     // we add more models here when we create them
 ];
 
 // We define all models according to their files.
 for (const modelDefiner of modelDefiners) {
-	modelDefiner(sequelize);
+    modelDefiner(sequelize);
 }
 
-// We export the sequelize connection instance to be used around our app.
+// // We export the sequelize connection instance to be used around our app.
 module.exports = sequelize;
+
+async function CreateTables() {
+    (async () => {
+        await sequelize.sync();
+        console.log('Tables created!');
+    })();
+}
+
